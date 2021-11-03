56fc3b114831cd81de29501e035105689e8f4cc1
rakoop <57457046+rakoop@users.noreply.github.com>
2021-11-03 17:12:11 -0500

    Update ManagerMenuState.java

--- ManagerMenuState.java
+++ ManagerMenuState.java
@@ -1,1 +1,12 @@
-
+/*******************************************************************************
+ManagerMenuState.java
+Responsible individual: 
+(a)    Add a product
+(b)    Add a supplier
+(c)     Show list of suppliers
+(d)    Show list of suppliers for a product, with purchase prices
+(e)    Show list of products for a supplier, with purchase prices
+(f)      Update products and purchase prices for a particular supplier. Actor provides supplierID; system displays a list of productIDs and purchase prices. Actor can remove product, add product or change purchase price.
+(g)    Become a salesclerk
+(h)    Logout.
+********************************************************************************/
