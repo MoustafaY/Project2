16494c1404a82b494850dca77063024fd4d524d8
rakoop <57457046+rakoop@users.noreply.github.com>
2021-11-03 17:13:36 -0500

    Update OpeningState.java

--- OpeningState.java
+++ OpeningState.java
@@ -1,1 +1,13 @@
-
+/*******************************************************************************
+OpeningState.java (Menu for System Essentially)
+Responsible individual: 
+Provides Interface to chose the users role
+	- typeing exit can be used to quit any state
+	- higher authorities may log into lower permissions
+		+ after exiting the user will return to the origional role
+Options:
+	1. Client
+	2. Clerk
+	3. Manager
+	q. quit
+********************************************************************************/
